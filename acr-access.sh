323d52dd8cbf7536832ccce78985758c2e9ea01c
Martijn Pepping <martijn.pepping@automiq.nl>
2018-11-13 20:50:17 +0100

    AKS deployment test

---
+++ acr-access.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Allow AKS to access ACR
+
+AKS_RESOURCE_GROUP=k8s-course
+AKS_CLUSTER_NAME=k8s-course
+ACR_RESOURCE_GROUP=k8s-course
+ACR_NAME=automiq
+
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
+
+# Create role assignment
+az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
